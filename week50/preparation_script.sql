473d7a44abdc275adc43501720804762b5fffdde
marco-scatassi-nimbus <161706034+marco-scatassi-nimbus@users.noreply.github.com>
2024-05-17 15:13:23 +0200

    Create preparation_script.sql

---
+++ week50/preparation_script.sql
@@ -0,0 +1,19 @@
+use database db_ff;
+create or replace schema week50;
+
+create table F_F_50 (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50)
+);
+insert into F_F_50 (id, first_name, last_name, email) values (1, 'Arly', 'Lissaman', 'alissaman0@auda.org.au');
+insert into F_F_50 (id, first_name, last_name, email) values (2, 'Cary', 'Baggalley', 'cbaggalley1@aol.com');
+insert into F_F_50 (id, first_name, last_name, email) values (3, 'Kimbell', 'Bertrand', 'kbertrand2@twitpic.com');
+insert into F_F_50 (id, first_name, last_name, email) values (4, 'Peria', 'Deery', 'pdeery3@addtoany.com');
+insert into F_F_50 (id, first_name, last_name, email) values (5, 'Edmund', 'Caselli', 'ecaselli4@prweb.com');
+insert into F_F_50 (id, first_name, last_name, email) values (6, 'Davin', 'Daysh', 'ddaysh5@liveinternet.ru');
+insert into F_F_50 (id, first_name, last_name, email) values (7, 'Starla', 'Legging', 'slegging6@soundcloud.com');
+insert into F_F_50 (id, first_name, last_name, email) values (8, 'Maud', 'Jaggers', 'mjaggers7@businesswire.com');
+insert into F_F_50 (id, first_name, last_name, email) values (9, 'Barn', 'Campsall', 'bcampsall8@is.gd');
+insert into F_F_50 (id, first_name, last_name, email) values (10, 'Marcelia', 'Yearn', 'myearn9@moonfruit.com');
